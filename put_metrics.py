99d1a3bac450cc8296835ae7af6736d3a7b37ea9
Michał Kosiński <michal.kosinski@gmail.com>
2020-10-24 16:18:49 +0200

    put into class

--- put_metrics.py
+++ put_metrics.py
@@ -1,61 +1,64 @@
-import boto3
+from boto3 import session
 from botocore.config import Config
 import logging
 import subprocess
 import time
 
-my_config = Config(
-    region_name='eu-west-1'
-)
 
-params = ["LINEV", "OUTPUTV", "LOTRANS", "HITRANS", "BATTV", "NOMOUTV", "NOMBATTV", "TIMELEFT", "LOADPCT", "ITEMP", "BCHARGE"]
+class PutUpsMetricsToCloudWatch:
+    my_config = Config(
+        region_name='eu-west-1'
+    )
 
-logging.basicConfig(level=logging.INFO)
-session = boto3.session.Session(profile_name='cloudwatch')
-cw = session.client('cloudwatch', config=my_config)
-
-
-def get_ups_data():
-    apcaccess_output = subprocess.check_output("apcaccess", shell=True, encoding='utf8')
-    apcaccess_result = {}
-    for row in apcaccess_output.split('\n'):
-        if ': ' in row:
-            key, value = row.split(': ')
-            apcaccess_result[key.strip(' .')] = value.strip()
-    return apcaccess_result
+    params = ["LINEV", "OUTPUTV", "LOTRANS", "HITRANS", "BATTV", "NOMOUTV", "NOMBATTV", "TIMELEFT", "LOADPCT", "ITEMP",
+              "BCHARGE"]
 
 
-def put_ups_metrics(params):
-    values = get_ups_data()
-    for param in params:
-        value = float(values[param].split(" ", 1)[0])
-        unit = values[param].split(" ", 1)[1]
-        logging.info(f"Unit for {param}: {unit}")
-        if unit == "Percent":
-            unit_for_metric = "Percent"
-        else:
-            unit_for_metric = "None"
-        response = cw.put_metric_data(
-            Namespace='Home/UPS/APC',
-            MetricData=[
-                {
-                    'MetricName': param,
-                    'Dimensions': [
-                        {
-                            'Name': 'Units',
-                            'Value': unit
-                        },
-                    ],
-                    'Value': value,
-                    'Unit': unit_for_metric,
-                },
-            ]
-        )
+    logging.basicConfig(level=logging.INFO)
+    session = session.Session(profile_name='cloudwatch')
+    cw = session.client('cloudwatch', config=my_config)
 
-        logging.info(f"Putting metric for param: {param} with unit: {unit_for_metric} and value: {value}")
-        logging.debug(f"API response: {response}")
+    def get_ups_metrics(self):
+        apcaccess_output = subprocess.check_output("apcaccess", shell=True, encoding='utf8')
+        apcaccess_result = {}
+        for row in apcaccess_output.split('\n'):
+            if ': ' in row:
+                key, value = row.split(': ')
+                apcaccess_result[key.strip(' .')] = value.strip()
+        return apcaccess_result
+
+    def put_ups_metrics(self, params):
+        ups_metrics = self.get_ups_metrics()
+        for param in params:
+            value = float(ups_metrics[param].split(" ", 1)[0])
+            unit = ups_metrics[param].split(" ", 1)[1]
+            logging.info(f"Unit for {param}: {unit}")
+            if unit == "Percent":
+                unit_for_metric = "Percent"
+            else:
+                unit_for_metric = "None"
+            response = self.cw.put_metric_data(
+                Namespace='Home/UPS/APC',
+                MetricData=[
+                    {
+                        'MetricName': param,
+                        'Dimensions': [
+                            {
+                                'Name': 'Units',
+                                'Value': unit
+                            },
+                        ],
+                        'Value': value,
+                        'Unit': unit_for_metric,
+                    },
+                ]
+            )
+
+            logging.info(f"Putting metric for param: {param} with unit: {unit_for_metric} and value: {value}")
+            logging.debug(f"API response: {response}")
 
 
-while True:
-    put_ups_metrics(params)
-    time.sleep(300)
+if __name__ == '__main__':
+    while True:
+        PutUpsMetricsToCloudWatch().put_ups_metrics(PutUpsMetricsToCloudWatch.params)
+        time.sleep(300)
